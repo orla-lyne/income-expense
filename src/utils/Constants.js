f2586347da9bfd0bbf239ee1ea744a55f2178210
unknown <kenneorla1@gmail.com>
2026-09-07 12:39:02 +0100

    chores; i added the different categories

---
+++ src/utils/Constants.js
@@ -0,0 +1,40 @@
+export const CATEGORIES = [
+    {
+        id: 'food',
+        name: 'Food',
+        
+    },
+    {
+        id: 'transport',
+        name: 'Transport',
+        
+    },
+    {
+        id: 'shopping',
+        name: 'Shopping',
+       
+    },
+    {
+        id: 'entertainment',
+        name: 'Entertainment',
+       
+    },
+    {
+        id: 'bills',
+        name: 'Bills',
+       
+    },
+    {
+        id: 'health',
+        name: 'Health',
+        
+    },
+    {
+        id: 'other',
+        name: 'Other',
+    
+    },
+];
+
+export const TYPES = { INCOME: 'income', EXPENSE: 'expense' };
+export const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
