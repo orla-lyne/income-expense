654d22cde0ab6caff6840ac61bef8e630ad700f0
unknown <kenneorla1@gmail.com>
2026-09-08 11:36:24 +0100

    feat:add app.jsx, app.css, useLocalStorage.js

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { AppProvider } from './context/AppContext';
+import { ThemeToggle } from './components/ThemeToggle';
+import Dashboard from './pages/Dashboard';
+import Transactions from './pages/Transactions';
+import Budgets from './pages/Budgets';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AppProvider>
+        <BrowserRouter>
+          <div className="app">
+            <aside className="sidebar">
+              <div className="logo">Expense Tracker</div>
+              <nav>
+                <Link to="/" className="link"> Dashboard</Link>
+                <Link to="/transactions" className="link"> Transactions</Link>
+                <Link to="/budgets" className="link"> Budgets</Link>
+              </nav>
+              <ThemeToggle />
+            </aside>
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/budgets" element={<Budgets />} />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </AppProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
